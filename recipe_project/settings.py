5c97ab6d2d4444f40d2c0d7a90b447969aaa93f1
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-09-29 17:49:00 -0400

    update settings

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -123,9 +123,7 @@ LOGIN_URL='/login/'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-   BASE_DIR / 'static'
-]
+
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
